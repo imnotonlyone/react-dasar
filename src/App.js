ab34014e6d9d29cee15742e8e6f22393032a6fa4
imnotonlyone <it.enginer001@gmail.com>
2022-01-10 21:26:39 +0700

    pertama

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Beranda from "./Component/Class/Beranda";
+import Home from "./Component/Fungsional/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <Beranda />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Class/Beranda.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Beranda extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ini adalah Beranda</h1>
+      </div>
+    );
+  }
+}
+
+export default Beranda;
