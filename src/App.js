373214cf28e47a9385f88ca060524c555458bbff
imnotonlyone <it.enginer001@gmail.com>
2022-01-12 22:06:55 +0700

    Parsing data props dan state antar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Parent from "./Component/Class/Parent";
 // import Beranda from "./Component/Class/Beranda";
-import Home from "./Component/Fungsional/Home";
+// import Home from "./Component/Fungsional/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Parent />
     </div>
   );
 }
